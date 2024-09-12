8f457fc46dbfbdef33e2c75d573a72d811ede079
mingderwang <mingderwang@me.com>
2024-09-11 17:44:58 -0700

    add swagger and elysia for v0.1.1

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,10 @@
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { Database } from "bun:sqlite";
-import { sql } from "drizzle-orm";
-import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
-import { Server } from "bun";
+import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
 
-export default {
-  async fetch(request: Request, server: Server) {
-    let text = "Hello from Bun on Vercel!\n";
-
-    const sqlite = new Database(":memory:");
-    const db = drizzle(sqlite);
-    const query = sql`select "hello ming2" as text`;
-    const result = db.get<{ text: string }>(query);
-    text += JSON.stringify(result);
-    text += `\nurl: ${request.url}\n`;
-
-    for (const [key, value] of request.headers.entries()) {
-     // if (!key.startsWith("x-vercel")) continue;
-      text += `\n${key}: ${value}`;
-    }
-
-    return new Response(text, {
-      status: 200,
-      headers: {
-        "Content-Type": "text/plain;charset=utf-8",
-      },
-    });
-  },
-};
+new Elysia()
+    .get('/', () => 'Hi')
+    .post('/hi', () => 'From Post')
+    .put('/hi', () => 'From Put')
+    .route('M-SEARCH', '/hi', () => 'Custom Method')
+    .use(swagger())
+    .listen(3000)
