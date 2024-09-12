7d0601a2781d7427dcc7800aada0b88e70d0270e
mingderwang <mingderwang@me.com>
2024-09-11 17:35:43 -0700

    🥭	🍧🧋🍻💭

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,23 @@
-import { drizzle } from 'drizzle-orm/bun-sqlite';
-import { Database } from 'bun:sqlite';
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
 import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { Server } from "bun";
 
 export default {
   async fetch(request: Request, server: Server) {
-    let text = "Hello from Bun on Vercel!\n"
+    let text = "Hello from Bun on Vercel!\n";
 
-const sqlite = new Database(':memory:');
-const db = drizzle(sqlite);
-const query = sql`select "hello ming2" as text`;
-const result = db.get<{ text: string }>(query);
+    const sqlite = new Database(":memory:");
+    const db = drizzle(sqlite);
+    const query = sql`select "hello ming2" as text`;
+    const result = db.get<{ text: string }>(query);
     text += JSON.stringify(result);
-    text += `\nurl: ${request.url}\n`
+    text += `\nurl: ${request.url}\n`;
 
     for (const [key, value] of request.headers.entries()) {
-      if (!key.startsWith("x-vercel")) continue
-      text += `\n${key}: ${value}`
+     // if (!key.startsWith("x-vercel")) continue;
+      text += `\n${key}: ${value}`;
     }
 
     return new Response(text, {
@@ -24,6 +25,6 @@ const result = db.get<{ text: string }>(query);
       headers: {
         "Content-Type": "text/plain;charset=utf-8",
       },
-    })
+    });
   },
-}
+};
